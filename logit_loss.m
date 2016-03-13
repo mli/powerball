713e6c5a375ad316306bd8aeffaa14f69369eedc
muli <muli@cs.cmu.edu>
2016-03-13 19:59:01 -0400

    update

--- logit_loss.m
+++ logit_loss.m
@@ -7,8 +7,7 @@ function [objv, grad] = logit_loss(Y, X, w, l2)
 % l2    : l2 regularizer
 % objv  : the objective value
 % grad  : the gradient
-
 tau = Y .* (X * w);
-objv = sum(log(1 + exp(-tau)));
 tau = max(-100, min(100, tau));
+objv = sum(log(1 + exp(-tau)));
 grad = X' * (- Y ./ (1  + exp(tau))) + l2 * w;
